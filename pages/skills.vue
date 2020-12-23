3051344c198980d8f12b07398e3efd7a02db2667
Fagner <fagner.txt@gmail.com>
2020-12-23 19:35:33 -0300

    finish skills page

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,6 +1,171 @@
 <template>
+  <!-- eslint-disable -->
   <v-row class="main-section" justify="center" align="center">
-    teste
+    <section class="skills">
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://www.w3schools.com/js/DEFAULT.asp"
+          target="_blank"
+        >
+          <img src="~/static/icons/js.svg" alt="Javascript" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://www.w3schools.com/html/"
+          target="_blank"
+        >
+          <img src="~/static/icons/html.svg" alt="HTML" class="image">
+
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://www.w3schools.com/css/"
+          target="_blank"
+        >
+          <img src="~/static/icons/css.svg" alt="CSS" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://vuejs.org/"
+          target="_blank"
+        >
+          <img src="~/static/icons/vue.svg" alt="Vue" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://nuxtjs.org/"
+          target="_blank"
+        >
+          <img src="~/static/icons/nuxt.svg" alt="Nuxt" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://vuetifyjs.com/en/"
+          target="_blank"
+        >
+          <img src="~/static/icons/vuetify.svg" alt="Vuetify" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://www.php.net/manual/pt_BR/intro-whatis.php"
+          target="_blank"
+        >
+          <img src="~/static/icons/php.svg" alt="PHP" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://br.wordpress.com/"
+          target="_blank"
+        >
+          <img src="~/static/icons/wordpress.svg" alt="WordPress" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://webpack.js.org/"
+          target="_blank"
+        >
+          <img src="~/static/icons/webpack.svg" alt="WebPack" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://www.npmjs.com/"
+          target="_blank"
+        >
+          <img src="~/static/icons/npm.svg" alt="NPM" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://git-scm.com/"
+          target="_blank"
+        >
+          <img src="~/static/icons/git.svg" alt="Git" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://github.com/"
+          target="_blank"
+        >
+          <img src="~/static/icons/github.svg" alt="GitHub" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://sass-lang.com/"
+          target="_blank"
+        >
+          <img src="~/static/icons/sass.svg" alt="SASS" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://getbootstrap.com.br/"
+          target="_blank"
+        >
+          <img src="~/static/icons/bootstrap.svg" alt="Bootstrap" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://www.figma.com/"
+          target="_blank"
+        >
+          <img src="~/static/icons/figma.svg" style="width: 75%;" alt="Figma" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://aws.amazon.com/pt/"
+          target="_blank"
+        >
+          <img src="~/static/icons/aws.svg" alt="AWS" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://www.docker.com/"
+          target="_blank"
+        >
+          <img src="~/static/icons/docker.svg" alt="Docker" class="image">
+        </a>
+      </div>
+      <div class="skills__item">
+        <a
+          class="card-link"
+          href="https://kubernetes.io/pt/"
+          target="_blank"
+        >
+          <img src="~/static/icons/kubernetes.svg" alt="Kubernetes" class="image">
+        </a>
+      </div>
+    </section>
   </v-row>
 </template>
 
@@ -15,5 +180,44 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.skills {
+  padding-top: 4rem;
+  width: 90%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
 
+  &__item {
+    width: 100px;
+    height: 100px;
+    margin: .5rem;
+    border-radius: $radius-normal;
+    background-color: $gray-800;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    &:hover {
+      .card-link {
+        .image {
+          filter: grayscale(0);
+          transition: .3s;
+        }
+      }
+    }
+
+    .card-link {
+      .image {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        padding: .9rem;
+        filter: grayscale(1);
+        transition: .3s;
+        margin: auto;
+      }
+    }
+  }
+}
 </style>
